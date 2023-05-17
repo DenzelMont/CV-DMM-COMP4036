1318cb0712f7384a0efbea0f77384a8ff90991ab
Denzel Montes-Melendez <65861688+DenzelMont@users.noreply.github.com>
2023-05-16 21:21:24 -0400

    Update CV.js

--- Den/CV.js
+++ Den/CV.js
@@ -1,47 +1,51 @@
+// Definición de la clase CV
 class CV {
-  constructor(name, email, phone, pExperience, coursework, edu, tskills, skills, lang, awards) {
-    this.name = name;
-    this.email = email;
-    this.phone = phone;
-    this.pExperience = pExperience;
-    this.coursework = coursework;
-    this.edu = edu;
-    this.tskills = tskills;
-    this.skills = skills;
-    this.lang = lang;
-    this.awards = awards;
+  // Constructor para inicializar los datos del CV
+  constructor(nombre, email, telefono, experienciaP, cursosRelevantes, edu, habilidadesT, lang, reconocimientos) {
+    this.nombre = nombre; // Mi nombre jaj
+    this.email = email; // Mi correo electronico
+    this.telefono = telefono; // Mi Teléfono
+    this.experienciaP = experienciaP; // Experiencia profesional del candidato
+    this.cursosRelevantes = cursosRelevantes; // Cursos relevantes que el candidato ha tomado
+    this.edu = edu; // Información de educación del candidato
+    this.habilidadesT = habilidadesT; // Habilidades técnicas del candidato
+    this.lang = lang; // Idiomas que habla el candidato
+    this.reconocimientos = reconocimientos; // Reconocimientos obtenidos por el candidato
   }
 
-  getName() {
-    document.writeln('<div id=' + '"' + 'contenido' + '"' + '><center><body style=background-color:f6f1de;><h1><b>' + this.name + ' </b></h1>');
+   // Funciones getter para obtener los datos del CV
+  getnombre() { // Función para obtener el nombre
+    document.writeln('<div id=' + '"' + 'contenido' + '"' + '><center><body style=background-color:f6f1de;><h1><b>' + this.nombre + ' </b></h1>');
   }
 
-  getEmail() {
+ // Y así sucesivamente para el resto de los datos...
+  
+ getEmail() {
     document.writeln(this.email);
     document.write(' | ');
   }
 
-  getphone() {
-    document.writeln(this.phone);
+  gettelefono() {
+    document.writeln(this.telefono);
     document.writeln('<hr>');
   }
 
-  getpExperience() {
+  getexperienciaP() {
     document.writeln('<h2><b>Professional Experience </b></h2>');
-    for (let i = 0; i < this.pExperience.length; i++) {
-      document.writeln(this.pExperience[i] + '<br>');
+    for (let i = 0; i < this.experienciaP.length; i++) {
+      document.writeln(this.experienciaP[i] + '<br>');
     }
     document.writeln('<hr>');
   }
 
   getCour() {
-    document.writeln('<h2><b>Relevant Coursework</b></h2><br>' + this.coursework + '<hr>');
+    document.writeln('<h2><b>Relevant Coursework</b></h2><br>' + this.cursosRelevantes + '<hr>');
   }
 
-  getTskills() {
+  gethabilidadesT() {
     document.writeln('<h2><b>Technical Skills & Tools</b></h2>');
-    for (let i = 0; i < this.tskills.length; i++) {
-      document.writeln(this.tskills[i] + '<br>');
+    for (let i = 0; i < this.habilidadesT.length; i++) {
+      document.writeln(this.habilidadesT[i] + '<br>');
     }
     document.writeln('<hr>');
   }
@@ -63,24 +67,6 @@ class CV {
     }
   }
 
-  getSkills(is_last = false) {
-    if (is_last == false) {
-      document.writeln('<h2><b>WHAT </b></h2>');
-      for (let i = 0; i < this.skills.length; i++) {
-        document.writeln(this.skills[i] + '<br>');
-      }
-      document.writeln('<hr>');
-    }
-    if (is_last) {
-      document.writeln('<div class="lang"><h2><b>Skills </b></h2>');
-      for (let i = 0; i < this.skills.length; i++) {
-        document.writeln(this.skills[i] + '<br>');
-      }
-
-      document.writeln('</div>');
-    }
-  }
-
   getlang(last = true) {
     if (last) {
       document.writeln('<div class="lang"><h2><b>Languages</b></h2>');
@@ -97,80 +83,73 @@ class CV {
     }
   }
 
-  setName(name) {
-    this.name = name;
+ // Funciones setter para establecer los datos del CV
+  setnombre(nombre) { // Función para establecer el nombre
+    this.nombre = nombre;
   }
+
+  // Y así sucesivamente para el resto de los datos...
 
   setEmail(email) {
     this.email = email;
   }
 
-  setPhone(phone) {
-    this.phone = phone;
+  settelefono(telefono) {
+    this.telefono = telefono;
   }
 
-  setpExperience(ed) {
-    this.pExperience = ed;
+  setexperienciaP(ed) {
+    this.experienciaP = ed;
   }
 
-  setCour(coursework) {
-    this.coursework = coursework;
+  setCour(cursosRelevantes) {
+    this.cursosRelevantes = cursosRelevantes;
   }
 
-  setTskills(exp) {
-    this.tskills = exp;
-  }
-
-  setSkills(skills) {
-    this.skills = skills;
+  sethabilidadesT(exp) {
+    this.habilidadesT = exp;
   }
 
   setlang(lang) {
     this.lang = lang;
   }
 
-  setAwards(awards) {
-    this.awards = awards;
+  setreconocimientos(reconocimientos) {
+    this.reconocimientos = reconocimientos;
   }
 
-  show(include_name = true, include_email = true, include_phone = true, include_obj = true, include_pExperience = true, include_exp = true, include_abil = true, include_skills = true, include_lang = true, include_awards = true, last_lang = true) {
-    if (include_name) {
-      this.getName();
+   // Función para mostrar los datos del CV
+  show(include_nombre = true, include_email = true, include_telefono = true, include_obj = true, include_experienciaP = true, include_exp = true, include_abil = true, include_lang = true, include_reconocimientos = true, last_lang = true) {
+    if (include_nombre) {
+      this.getnombre();
     }
+     // Y así sucesivamente para el resto de los datos...
     if (include_email) {
       this.getEmail();
     }
-    if (include_phone) {
-      this.getphone();
+    if (include_telefono) {
+      this.gettelefono();
     }
     if (include_obj) {
       this.getCour();
     }
     if (include_exp) {
-      this.getTskills();
+      this.gethabilidadesT();
     }
-    if (include_pExperience) {
-      this.getpExperience();
+    if (include_experienciaP) {
+      this.getexperienciaP();
     }
     if (include_abil) {
       this.getedu();
     }
-    if (include_skills && !include_lang) {
-      if (include_skills) {
-        this.getSkills(true);
-      } else {
-        this.getSkills(false);
-        this.getlang();
-      }
-    }
     if (include_lang) {
       this.getlang(last_lang);
     }
-    if (include_awards) {
+    if (include_reconocimientos) {
       document.writeln('<h2><b>Awards</b></h2>');
       
-      for (let i = 0; i < this.awards.length; i++) {
-        document.writeln(this.awards[i] + '<br>');
+      for (let i = 0; i < this.reconocimientos.length; i++) {
+        document.writeln(this.reconocimientos[i] + '<br>');
       }
       
 
@@ -179,27 +158,27 @@ class CV {
 }
 
 
-const name = 'Denzel Montes-Melendez';
+const nombre = 'Denzel Montes-Melendez';
 const email = 'denzel.montes@upr.edu';
-const phone = '(787) 371-7499';
-const pExperience = ['Administrative Assistant | University of Puerto Rico - Mayaguez | Mayaguez, PR', 'For the 2022-2023 academic year, I was chosen to participate in the Federal Work-Study Program, which provides college students with a job in their university offices.',
+const telefono = '(787) 371-7499';
+const experienciaP = ['Administrative Assistant | University of Puerto Rico - Mayaguez | Mayaguez, PR', 'For the 2022-2023 academic year, I was chosen to participate in the Federal Work-Study Program, which provides college students with a job in their university offices.',
 ' ', 'Website Team Member | Feria de Investigaciones UPRM | Mayaguez, PR', 'Selected to collaborate on the website team at the University of Puerto Rico – Mayaguez research fair for the 2022-2023 academic year',
 ];
-const coursework = 'Computer Organization | Linear Algebra | Programming Languages (In Progress)';
+const cursosRelevantes = 'Computer Organization | Linear Algebra | Programming Languages (In Progress)';
 const edu = ['Bachelor of Science in Computer Science, University of Puerto Rico - Mayagüez, 2020-2025','GPA: 3.82/4.00', ' ', 'Tech Mini-Mester Fellow | Capital One', 'Selected & participated in an 8-day immersive experience for Capital One\'s Tech Mini-Mester to enhance tech skills, soft skills and mentorship. July 2022 - August 2022'];
-const tskills = ['C++ | Python | HTML | git | Visual Studio Code | Object Oriented Programming | OpenFrameWorks | Dev C++ | Microsoft Word | Power Point | WordPress | Practical experience assembling computers and 3D printers from parts.'];
-const skills = ['Object-oriented programming', 'Web development', 'Database management', 'Problem-solving'];
+const habilidadesT = ['C++ | Python | HTML | git | Visual Studio Code | Object Oriented Programming | OpenFrameWorks | Dev C++ | Microsoft Word | Power Point | WordPress | Practical experience assembling computers and 3D printers from parts.'];
 const lang = ['Fully billingual in: English and Spanish'];
-const awards = ['Honors Student at the University of Puerto Rico - Mayaguez: 2021-2022 & 2022-2023', 'Hispanic Scholarship Fund Scholar: 2022-2023'];
+const reconocimientos = ['Honors Student at the University of Puerto Rico - Mayaguez: 2021-2022 & 2022-2023', 'Hispanic Scholarship Fund Scholar: 2022-2023'];
 
-const myCV = new CV(name, email, phone, pExperience, coursework, edu, tskills, skills, lang, awards);
-
-
+const myCV = new CV(nombre, email, telefono, experienciaP, cursosRelevantes, edu, habilidadesT, lang, reconocimientos);
 
 
 
+
+// Llamada a la función show para mostrar los datos del CV
 myCV.show();
 
+// Función para descargar el CV como un archivo PDF
 function download() {
   var link = document.createElement('a');
   link.href = "CV.pdf";
